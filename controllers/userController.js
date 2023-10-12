0129ef5a4ee2f0dce87909f4fdaef6f89533371c
Abhyanshu Pandey <abhyanshupandeyji@gmail.com>
2023-10-12 18:21:18 +0530

    the register function created in auth controller , the database is just simple connection , only difference is that we need to import its connection in every file , instead like mongodb where we only need it to import it in main server, but thats due to mysql db queries to use the db connection for every query function  , look the video further to see some already solved problems because instructor can import or install things without showing it eariler

---
+++ controllers/userController.js
@@ -0,0 +1,9 @@
+
+
+
+
+
+// User 
+export const getUser = (req,res)=>{
+    res.send("api request in user routes works !");
+}
---
+++ routes/posts.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+
+// controller import 
+import {  } from '../controllers/postController.js'
+
+const router = express.Router();
+
+
+
+router.get("" ,  )  
+
+
+
+export default router
