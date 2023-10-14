5a84563740028f7ce4ede268de80e9951fb2baa9
Abhyanshu Pandey <abhyanshupandeyji@gmail.com>
2023-10-14 11:09:55 +0530

    created getposts route , to get post , we need to first take the post and then join the table to the user id so the user who is logged in can see it , now id in the post will be used to access user id who posted it , and logged in user id will be used to let the post to be seen , if you only use the post data ,and the user id you can see all posts , we take every thing from the post , and join it with the user id of the data base to link it to the post to show the user id and posts linked to it- with this method we can create page where we want to see all the post on the app , and then we use the relationship to show the user only the post of the user he follow and the user itself , thanks to the left join of my sql

--- APIFutureUpdateReadme.md
+++ APIFutureUpdateReadme.md
@@ -2,3 +2,11 @@
 
 
 #### We talk about What can be done with this and can be updated with this in the future 
+
+
+- can add an explore page where we can have seen all the posts from all the users on the internet - for that in my sql
+```
+SELECT p.*, u.id AS userId , name , profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId)
+
+use this line it will only link the posts with the user , and not specific post followed by the users helps in explore pages , the posts.userId if given null will show all the post , and user with id can see it , but if the user is not logged in it will not thanks to the help of jwt verification 
+```
---
+++ controllers/postController.js
@@ -0,0 +1,45 @@
+import  db  from '../config/connect.js'
+import cookies from "cookie-parser";
+// mf jwt import creating error - wow have some ability to debug the app
+import jwt from 'jsonwebtoken'
+
+
+// to get the posts - to see only our friends posts and our posts ( excluding the all posts like explore feature )
+export const getPosts = (req ,res) =>{
+
+    const token  = req.cookies.accessToken;
+
+    if(!token)  return res.status(401).json("not logged in!");
+    
+
+
+    jwt.verify(token , "secretKey" , (err , userInfo)=>{
+        if(err) return res.status(403).json("Token is not Valid!");
+    
+        
+        // AS for short form for noting the posts as p & users as u , and join helps to join both table 
+        // but why - to show our user also on the post made 
+        // to prevent conflict between post id and user id , we use u.id ad userId - don't get confused the p.userId is still userId saved in the posts
+        
+        // this part is not a problem but the token is not coming - question marks take our user id
+        const q = `SELECT p.*, u.id AS userId , name , profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId)
+        LEFT JOIN relationships As r ON (p.userId = r.followedUserId) WHERE r.followerUserId = ? OR p.userId = ?
+        ORDER BY p.createdAt DESC`;
+        // left join help to take both our posts and followed users posts
+    
+        db.query(q , [userInfo.id , userInfo.id],  (err ,data)=>{
+
+            if(err) return res.status(500).json(err);
+            return res.status(200).json(data);
+        });
+    
+    });
+
+
+
+}
+
+
+export const addPost = (req , res )=>{
+    
+}
